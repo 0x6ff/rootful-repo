36285a18773ca748454054b5ae2bf3af2fdebb6a
vixoarchive <btebusinessp@gmail.com>
2023-01-13 13:16:03 +0000

    major updates

--- repo.sh
+++ repo.sh
@@ -10,14 +10,14 @@ bzip2 -c9 Packages > Packages.bz2
 
 echo "[Repository] Generating Release..."
 apt-ftparchive \
-		-o APT::FTPArchive::Release::Origin="Dev Team" \
-		-o APT::FTPArchive::Release::Label="Dev Team" \
+		-o APT::FTPArchive::Release::Origin="iPhone Dev Team" \
+		-o APT::FTPArchive::Release::Label="iPhone Dev Team" \
 		-o APT::FTPArchive::Release::Suite="stable" \
 		-o APT::FTPArchive::Release::Version="3.0" \
 		-o APT::FTPArchive::Release::Codename="ios" \
 		-o APT::FTPArchive::Release::Architectures="iphoneos-arm" \
 		-o APT::FTPArchive::Release::Components="main" \
-		-o APT::FTPArchive::Release::Description="A parody of the ultrasn0w repo." \
+		-o APT::FTPArchive::Release::Description="A parody of the old Dev Team repo which had ultrasn0w." \
 		release . > Release
 
 echo "[Repository] Signing Release using Azreal's GPG Key..."
